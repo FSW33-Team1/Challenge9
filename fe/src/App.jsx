c0b97d71701a3581aec240a573d5f5410043ade9
Davitaridani <davitaridani@gmail.com>
2023-08-06 21:48:19 +0700

    delete file

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Landing from "./pages/landing";
-import Register from "./pages/register";
-import Login from "./pages/login";
-import Home from "./pages/home";
-import Profile from "./pages/profile";
-import GameList from "./pages/gamelist";
-import Game from "./pages/game";
-
-class App extends Component {
-  render(){
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/game-list" element={<GameList/>}/>
-          <Route path="/game" element={<Game/>}/>
-        </Routes>
-      </Router>
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router >
+  );
 }
 
 export default App
--- fe/src/view/GameView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GameView() {
-  return (
-    <div>GameView</div>
-  )
-}
-
-export {GameView}
