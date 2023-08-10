0496da7e9a8ed20fb4686c5998d9df21c40310b0
fahrysrg <fahryrozysrg@gmail.com>
2023-08-10 08:26:05 +0700

    setup readme doc

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const controller = require("../controller/user_controller");
+const UserController = new controller();
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/:id", UserController.findUser);
 
 module.exports = router;
