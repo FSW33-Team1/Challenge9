ca0af4cd3d3d2fdd39486a769d507b8863a8c31a
Davitaridani <davitaridani@gmail.com>
2023-08-14 20:20:39 +0700

    revisi link

--- fe/src/components/UI/Card.jsx
+++ fe/src/components/UI/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ item }) => {
 					<div className="card-body">
 						<h5>{item.title}</h5>
 					</div>
-					<Link to="/game-detail" className=' d-flex justify-content-center'>View Detail</Link>
+					<Link to="/detail" className=' d-flex justify-content-center'>View Detail</Link>
 				</div>
 			</div>
 		</>
