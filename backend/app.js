b1b55f95456924b547e97c8f18897196c77247da
dickykintano1 <60821382+dickykintano1@users.noreply.github.com>
2023-08-23 22:02:01 +0700

    Deploy attempts
    FE is now online

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require("cors");
 
 const app = express();
 var corsOptions = {
